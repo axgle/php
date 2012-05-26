7575a1d3b9393052e2b57df48b1314e39d97d54f
haoqis <haoqis@gmail.com>
2012-05-26 18:14:09 +0800

    '.'

---
+++ github/oauth/callback.php
@@ -0,0 +1,42 @@
+<pre>
+<?php
+//http://developer.github.com/v3/oauth/
+error_reporting(E_ALL);
+if(isset($_REQUEST['code'])){
+	print_r([ $_REQUEST,
+	// $_SERVER
+	]);
+
+ 
+
+$q=vpost("https://github.com/login/oauth/access_token",array(
+	 'code'=>$_REQUEST['code'],
+	 'client_id'=>'29dbb97c69ec44f8f62b',
+	 'client_secret'=>''
+	));
+	 
+}
+
+//header("location:https://api.github.com/user?$q");
+
+function vpost($url,$data){ // 模拟提交数据函数
+    $curl = curl_init(); // 启动一个CURL会话
+    curl_setopt($curl, CURLOPT_URL, $url); // 要访问的地址
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 1); // 从证书中检查SSL加密算法是否存在
+    curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // 模拟用户使用的浏览器
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); // 使用自动跳转
+    curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // 自动设置Referer
+    curl_setopt($curl, CURLOPT_POST, 1); // 发送一个常规的Post请求
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data); // Post提交的数据包
+    curl_setopt($curl, CURLOPT_TIMEOUT, 30); // 设置超时限制防止死循环
+    curl_setopt($curl, CURLOPT_HEADER, 0); // 显示返回的Header区域内容
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 获取的信息以文件流的形式返回
+    $tmpInfo = curl_exec($curl); // 执行操作
+    if (curl_errno($curl)) {
+       echo 'Errno'.curl_error($curl);//捕抓异常
+    }
+    curl_close($curl); // 关闭CURL会话
+    return $tmpInfo; // 返回数据
+}
+?>
